874cab41614b3abd260b6a3f858311217745dfd7
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:32:22 -0700

    Update PIPOregister.v

--- Project 7/PIPOregister.v
+++ Project 7/PIPOregister.v
@@ -1,8 +1,16 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: Parallel In - Parallel Out Register
+// Module Name: PIPOregister
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
-module PIPOregister(input clk, input b, input [7:0] D, output reg [7:0] O);
+module PIPOregister(input clk, input EN, input [7:0] Data, output reg [7:0] Out);
 
 always @(posedge(clk)) begin
-if(b) O <= D;
+  if(EN) Out <= Data;
 end
+  
 endmodule
