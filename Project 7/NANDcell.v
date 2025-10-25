29149ddcae7d9d94df7418dd412c21767b2a68df
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:30:03 -0700

    Update NANDcell.v

--- Project 7/NANDcell.v
+++ Project 7/NANDcell.v
@@ -1,5 +1,11 @@
 `timescale 1ns / 1ps
-
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: NAND Basic Cell
+// Module Name: NANDcell
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
 module NANDcell(input s, input r, output Q1, output Q2);
 assign #10 Q2 = !(Q1&s);
