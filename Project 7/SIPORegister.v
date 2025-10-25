63f3220aeecd4633eba2235467d3cadfd8b52171
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:34:49 -0700

    Update SIPORegister.v

--- Project 7/SIPORegister.v
+++ Project 7/SIPORegister.v
@@ -1,6 +1,13 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: Series In - Parallel Out Register
+// Module Name: SIPOregister
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
-module SIPORegister(input clk, input SDI, output reg [7:0] Out);
+module SIPOregister(input clk, input SDI, output reg [7:0] Out);
 
 always @(posedge(clk)) begin
 Out <= {Out[6:0], SDI};
