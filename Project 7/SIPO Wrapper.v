617ef87226682c8917ab242fa08fef8731d7ce24
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:34:13 -0700

    Update SIPO Wrapper.v

--- Project 7/SIPO Wrapper.v
+++ Project 7/SIPO Wrapper.v
@@ -1,4 +1,11 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: Series In - Parallel Out Register Application Wrapper
+// Module Name: SIPOwrapper
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
 module SIPOWrapper(input clk, input btn, output [7:0] led);
 
@@ -7,6 +14,6 @@ reg [25:0] cntr;
  always @ (posedge(clk))
     cntr <= cntr + 1;
     
-SIPORegister SIPO_reg(.clk(cntr[25]), .SDI(btn), .Out (led));
+SIPORegister SIPO_reg(.clk(cntr[25]), .SDI(btn), .Out(led));
 
 endmodule
