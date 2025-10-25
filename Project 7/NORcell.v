b4c423a75c2bd0f2cd7f8082a86a61559775440f
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:30:55 -0700

    Update NORcell.v

--- Project 7/NORcell.v
+++ Project 7/NORcell.v
@@ -1,4 +1,11 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: NOR Basic Cell
+// Module Name: NORcell
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
 module NORcell(input s, input r, output Q1, output Q2);
 assign #10 Q2 = !(Q1&s);
