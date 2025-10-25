cd24694e454d14b479366f4f4d965d6ed3711122
Gus Ballman <gus.ballman@wsu.edu>
2025-10-25 16:33:27 -0700

    Update PIPOwrapper.v

--- Project 7/PIPOwrapper.v
+++ Project 7/PIPOwrapper.v
@@ -1,9 +1,16 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Gus Ballman
+// Create Date: 10/23/2025
+// Design Name: Parallel In - Parallel Out Register Application Wrapper
+// Module Name: PIPOwrapper
+// Project Name: EE 214 Project 7
+//////////////////////////////////////////////////////////////////////////////////
 
 module PIPOwrapper(input [7:0] sw,input clk, input [1:0] btn, output reg [7:0] led);
     
 wire [7:0] O;
-PIPOregister PIPO_init(.clk(clk), .b(btn[0]), .D(sw), .O(O));
+    PIPOregister PIPO_init(.clk(clk), .EN(btn[0]), .Data(sw), .Out(O));
 
 always @(*) begin
 case (btn[1])
